7ff4e425eb943036b53e1db8f426fbd9ad44a2ab
valtteri.puonti <valtteri.puonti@lamia.fi>
2020-09-23 10:59:30 +0300

    Project setup

---
+++ app/src/main/java/fi/vpuonti/textinputlayoutbug/compound/LoadingInput.kt
@@ -0,0 +1,13 @@
+package fi.vpuonti.textinputlayoutbug.compound
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class LoadingInput @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleSet: Int = 0
+) : LinearLayout(context, attrs, defStyleSet) {
+
+}
